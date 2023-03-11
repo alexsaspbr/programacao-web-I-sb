9ee35da7c4e620a348798e1c6e1ff1d24d5f76ca
Alex Araujo <alexsaspbr@gmail.com>
2023-03-10 23:39:58 -0300

    dockerizando a aplicacao

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,8 @@
 INSERT INTO tb_veiculo(placa, marca, modelo, tipo, disponivel) values ('XCV1548', 'Ford', 'Ford KA', 'Sedan', true );
+INSERT INTO tb_veiculo(placa, marca, modelo, tipo, disponivel) values ('XCV2548', 'Ford', 'Ford KA', 'Sedan', true );
+INSERT INTO tb_veiculo(placa, marca, modelo, tipo, disponivel) values ('XCV3548', 'Ford', 'Ford KA', 'Sedan', true );
+INSERT INTO tb_veiculo(placa, marca, modelo, tipo, disponivel) values ('XCV4548', 'Ford', 'Ford KA', 'Sedan', true );
+INSERT INTO tb_veiculo(placa, marca, modelo, tipo, disponivel) values ('XCV5548', 'Ford', 'Ford KA', 'Sedan', true );
+INSERT INTO tb_veiculo(placa, marca, modelo, tipo, disponivel) values ('XCV6548', 'Ford', 'Ford KA', 'Sedan', true );
+INSERT INTO tb_veiculo(placa, marca, modelo, tipo, disponivel) values ('XCV7548', 'Ford', 'Ford KA', 'Sedan', true );
+INSERT INTO tb_veiculo(placa, marca, modelo, tipo, disponivel) values ('XCV8548', 'Ford', 'Ford KA', 'Sedan', true );
